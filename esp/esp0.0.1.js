44c408f1a209401bcbdb71bdc902f76bff3ba03d
Abhinav Sinha <abhinavsinha001@gmail.com>
2021-04-07 14:58:27 +0530

    Update esp0.0.1.js

--- esp/esp0.0.1.js
+++ esp/esp0.0.1.js
@@ -1,31 +1,34 @@
 function fetchAsyncSignals(mode) {
-        class Signals {
-           constructor(signal) {
+    class Signals {
+        constructor(signal) {
             this.signal_string = signal;
-           }
-           encrypt() {
-            return btoa(this.signal_string)
-          }
         }
-        var promise;
-        switch (mode) {
-        case 1:
-          var eids=""
-          if(pbjs && pbjs.getUserIdsAsEids && typeof pbjs.getUserIdsAsEids === "function"){
-            eids=pbjs.getUserIdsAsEids();
-          }
-          break;
-        case 2:
-          var eids=""
-          if(owpbjs && owpbjs.getUserIdsAsEids && typeof owpbjs.getUserIdsAsEids === "function"){
-            eids=owpbjs.getUserIdsAsEids();
-          }
-          let signals= new Signals(JSON.stringify(eids)) // Get IDs from PubMatic Identity Hub
-          promise = Promise.resolve(signals); 
-          break;
-        default:
-          let signals= new Signals("Hello eids") // Demo data
-          promise = Promise.resolve(signals);         
-      }
-      return promise;
-};
+        encrypt() {
+            return btoa(this.signal_string)
+        }
+    }
+    var promise;
+    let signals = new Signals("");
+    switch (mode) {
+    case 1:
+        var eids = ""
+        if (pbjs && pbjs.getUserIdsAsEids && typeof pbjs.getUserIdsAsEids === "function") {
+            eids = pbjs.getUserIdsAsEids();
+        }
+        break;
+    case 2:
+        var eids = ""
+        if (owpbjs && owpbjs.getUserIdsAsEids && typeof owpbjs.getUserIdsAsEids === "function") {
+            eids = owpbjs.getUserIdsAsEids();
+        }
+        signals = new Signals(JSON.stringify(eids))
+        // Get IDs from PubMatic Identity Hub
+        promise = Promise.resolve(signals);
+        break;
+    default:
+        signals = new Signals("Hello eids");
+        // Demo data
+        promise = Promise.resolve(signals);
+    }
+    return promise;
+}
